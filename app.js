73385c0ab0a51acf19fef9c79e34ed10dc547eb3
Saadaf Chowdhury <saadaf.chowdhury@mail.utoronto.ca>
2019-06-17 16:19:50 -0400

    Add getComputerChoice ( ) function to app.js

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+
+function getComputerChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random()*3);
+    return choices[randomNumber];
+}
